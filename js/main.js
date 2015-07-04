409f97f36978cb8c09fb4ef579d2499a12a06644
rnyschyi <rnyschyy@gmail.com>
2015-07-04 18:03:21 +0300

    gallery add

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'});
+$(document).ready(function() {
 
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true});
+    $('.portfolio-thumb-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.portfolio-item-slider',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true
+    });
+
+    $('.portfolio-item-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.portfolio-thumb-slider'
+    });
+
+});
