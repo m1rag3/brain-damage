3eb272087e2ab4cc499d2231f5b273f5cea72a5b
m1rag3 <rpz01992@gmail.com>
2015-07-17 03:45:09 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	stylesheets/screen.css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-$(document).ready(function() {
+$('.portfolio-item-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.portfolio-thumb-slider'
+});
 
-    $('.portfolio-thumb-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.portfolio-item-slider',
-        dots: false,
-        arrows: false,
-        focusOnSelect: true
-    });
-
-    $('.portfolio-item-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        asNavFor: '.portfolio-thumb-slider'
-    });
-
+$('.portfolio-thumb-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.portfolio-item-slider',
+    dots: false,
+    arrows: false,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
